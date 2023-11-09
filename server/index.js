e1c3359d4c56c68b4e5abaf4d2a48fa42c75e4b1
grayna0 <grayna0164@gmail.com>
2023-11-09 15:48:29 +0700

    ttest

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+const server=app.listen(process.env.PORT,() =>{
+    console.log("listening on port " + process.env.PORT);
+})
